af8ad3ea4b6eb9c577fe903ab0ed02ae29eb15b8
MZayed47 <zayedmashrukh@gmail.com>
2022-02-24 12:03:41 +0600

    save recognized frames

--- Recognition_Project.py
+++ Recognition_Project.py
@@ -3,8 +3,12 @@ import numpy as np
 import face_recognition
 import os
 import csv
+from time import gmtime, strftime
 from datetime import datetime
 from PIL import ImageGrab
+
+
+yy = strftime("%d-%b-%Y_%H-%M", gmtime())
 
 
 # Source Images
@@ -50,14 +54,23 @@ print("Number of Records: ",len(encodeListKnown))
 cap = cv2.VideoCapture(0)
 
 
+frame_num = 0
+
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
+    frame_num += 1
+
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
- 
+
+    person_path = os.path.join(os.getcwd(), 'detections', 'person_' + yy)
+    isdir = os.path.isdir(person_path)
+    if not isdir:
+        os.mkdir(person_path)
+
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
@@ -67,12 +80,16 @@ while True:
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name,'\n')
-            
+
+            final_path = os.path.join(person_path, 'frame_' + str(frame_num) + '_' + name)
+
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,255,255),2)
+            cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,0.7,(255,255,255),2)
+
+            cv2.imwrite(final_path + '.jpg', img)
 
 
     cv2.imshow('Webcam',img)
